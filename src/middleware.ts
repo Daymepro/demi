37e99894ace6a25a8c3dbbfcc3a574ce1f09b480
Oyawoye Oluwademilade <OYAWOYEGALAXY@GMAIL.COM>
2024-03-18 07:32:22 +0100

    host name log fix for prod

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,11 +10,12 @@ export function middleware(request: NextRequest) {
   const pathWithSearchParams = `${url.pathname}${
     searchParams.length > 0 ? `${searchParams}` : ""
   }`;
-  
+  const Host = process.env.NEXT_PUBLIC_DOMAIN || "fluttersuite.com";
   const PUBLIC_FILE = /\.(.*)$/;
+  console.log(Host); 
   const customSubDomain = hostname
     .get("host")
-    ?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`)
+    ?.split(Host)
     .filter(Boolean)[0];
  
     if(PUBLIC_FILE.test(url.pathname) || url.pathname.includes('_next')) return
